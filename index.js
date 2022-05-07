8f4f0ddf6eaeb04391b1ab953aadc60e77287aae
Chiorean Bogdan <chioreanbogdangabriel@gmail.com>
2022-05-07 18:22:44 +0300

    did something

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,35 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+let object = {};
+
+const formatTime = (time) => time > 10 ? time : `0${time}`;
+app.get('/api/:input', (req, res) => {
+  const ceva = req.params.input ? req.params.input : Date.now()
+  if (isNaN(req.params.input)) return res.json({ error: "Invalid Date" });
+
+  const receivedDate = !isNaN(ceva) && parseInt(req.params.input, 10);
+
+  const a = new Date(parseInt(receivedDate, 10))
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  const year = a.getFullYear();
+  const month = months[a.getMonth()];
+  const date = a.getDate();
+  const hour = formatTime(a.getHours());
+  const min = formatTime(a.getMinutes());
+  const sec = formatTime(a.getSeconds());
+
+  res.json({ unix: receivedDate, utc: `${days[a.getDay()]}, ${date} ${month} ${year} ${hour}:${min}:${sec} GMT` })
 });
